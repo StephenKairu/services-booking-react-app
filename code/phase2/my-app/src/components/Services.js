d560ab1fe458e0dfeb4671bb59d46d213b446684
Stephen <skairu09@gmail.com>
2022-10-06 05:33:50 +0300

    Add text into services section

--- code/phase2/my-app/src/components/Services.js
+++ code/phase2/my-app/src/components/Services.js
@@ -1,4 +1,19 @@
 import React from "react";
 
 
-export default function Services() {}
+export default function Services() {
+
+    return(
+        <div>
+            <h2>Our Services</h2>
+            <h3>Top Hair Styles</h3>
+            <div className="display_images" id="container">
+            </div>
+            <div>
+            <h3 id="dye">Hair Dye Services</h3>
+            </div>
+            <div id="hair-dye" className="display_images">
+            </div>
+        </div>
+    )
+}
