90a5b6a3c829ac01e06270129e2bc95f1cc117c1
Stephen <skairu09@gmail.com>
2022-10-06 06:26:17 +0300

    Add contact details to footer component

--- code/phase2/my-app/src/components/Footer.js
+++ code/phase2/my-app/src/components/Footer.js
@@ -1,4 +1,32 @@
 import React from "react";
 
 
-export default function Footer() {}
+export default function Footer() {
+    return (
+        <div>Footer
+            <section class="footer">
+        <div class="box-container">
+
+
+    
+            <div class="footer-links">
+                <h3>Contact Us</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +2547-000-000-123 </a>
+                <a href="#"> <i class="fas fa-phone"></i> +065-111-222-333 </a>
+                <a href="#"> <i class="fas fa-envelope"></i> barbershop@gmail.com </a>
+                <a href="#"> <i class="fas fa-map-marker-alt"></i>Westlands- Nairobi, Kenya </a>
+            </div>
+    
+            <div class="footer-links">
+                <h3>Follow us</h3>
+                <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+                <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+                <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+                <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+            </div>
+    
+        </div>
+    </section>
+        </div>
+    )
+}
