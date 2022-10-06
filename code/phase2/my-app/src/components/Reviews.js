97bfbd1606f4ddb6c0a489fc269c06f2004190e2
Stephen <skairu09@gmail.com>
2022-10-06 06:06:19 +0300

    Add review form

--- code/phase2/my-app/src/components/Reviews.js
+++ code/phase2/my-app/src/components/Reviews.js
@@ -1,4 +1,23 @@
 import React from "react";
 
 
-export default function Reviews() {}
+export default function Reviews() {
+
+    return (
+        <div className="reviews">
+            <h2>Our Customers Say...</h2>
+ 
+                <div class="form">
+                    <h3>Satisfied with our service?</h3>
+                    <form id="comment-form" class="comment-form">
+                    Name: <br/><input type="text" name="name" placeholder="Your name?" id="client-name" required /> <br/> <br/>
+                    Email: <br/><input type="email" name="email" placeholder="Your email won't be published" class="email-input" required /> <br/><br/>
+                    Comment: <br/>
+                    <textarea name="body" id="client-text" cols="30" rows="6" placeholder="Leave a review..." required></textarea><br/><br/>
+                    <button type="submit" id="submit-button">Submit Review</button>
+                    </form>
+                </div>
+              
+        </div>
+    );
+}
