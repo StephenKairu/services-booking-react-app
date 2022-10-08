581a3cc448a949cd034164be4d8a3979088e7d6f
Stephen <skairu09@gmail.com>
2022-10-08 11:11:31 +0300

    Add image in Home

--- services-booking-react-app/src/components/Calendly.js
+++ services-booking-react-app/src/components/Calendly.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { InlineWidget } from 'react-calendly';
 
 const Calendly = () => {
-return <InlineWidget url="https://calendly.com/stevekairu/30min?hide_gdpr_banner=1" />
+    <div>
+        return <InlineWidget url="https://calendly.com/stevekairu/30min?hide_gdpr_banner=1" />
+    </div>
 }
 
 export default Calendly
--- services-booking-react-app/src/components/NavBar.js
+++ services-booking-react-app/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './Navbar.css'
+import Logo from '../assets/images/BarbershopLogo.jpg'
 
 export default function NavBar() {
 return (
     <div className="navbar">
+        {/* <img src={Logo} className="logo" /> */}
         <ul>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/services">Services</NavLink>
